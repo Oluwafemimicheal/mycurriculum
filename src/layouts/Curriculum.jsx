56c7edab78a56a8f31da6bce754fc7ed71c4df68
oluwafemi michael <femi4410@gmail.com>
2025-11-11 22:04:26 +0100

    initial commit

--- src/layouts/Curriculum.jsx
+++ src/layouts/Curriculum.jsx
@@ -16,7 +16,7 @@ const Curriculum = ({ handleLoginPopUp }) => {
       <motion.div initial={{ opacity: 0, x: -100 }}
         animate={{ opacity: 1, x: 0 }}
         exit={{ opacity: 0, x: 0 }}
-        transition={{ duration: 0.2 }} className="flex flex-col justify-center gap-30 lg:px-30 lg:py-10  py-10 px-0">
+        transition={{ duration: 0.2 }} className="flex flex-col justify-center gap-30 lg:px-30 lg:py-10  py-10 px-3">
         <div>
           <h1 className=" text-2xl lg:text-3xl font-bold mb-8"><span className="text-blue-500">Frontend Development</span> Curriculum</h1>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
